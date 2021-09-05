4903940cd5b1b7a65fe0abbf30d0248afc488a20
satwikn07 <satwikn07@gmail.com>
2021-09-05 12:22:38 +0530

    bars using canvas

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-var time = 0;
+var time = 0; //time lapsed is 0, initially
 // document.getElementById('showbars').addEventListener('click',showbars);
 var start_pause_button = document.getElementById('start-pause');
 start_pause_button.addEventListener('click',start_pause);
-function showbars(){
+function showbars(){ //this fuctions adds bars to the display
     let container = document.getElementById('container');
     container.textContent = "";
     for(let i=1;i<=90;i++){
-        let bar = document.createElement('div');
-        // bar.textContent = i;
-        bar.id = i;
-        bar.class = 'bars';
-        bar.style.backgroundColor = '#b295e9';
-        bar.style.minHeight = '30px';   
-        bar.style.height = `${Math.random()*100}px`;
-        bar.style.maxHeight = '100px';
-        bar.style.cursor = 'pointer';
-        bar.addEventListener('click',shift);
-        container.appendChild(bar);
+        // let bar = document.createElement('div');
+        // // bar.textContent = i;
+        // bar.id = i;
+        // bar.class = 'bars';
+        // bar.style.backgroundColor = '#b295e9';
+        // bar.style.minHeight = '30px';   
+        // bar.style.height = `${Math.random()*100}px`;
+        // bar.style.maxHeight = '100px';
+        // bar.style.cursor = 'pointer';
+        // bar.addEventListener('click',shift);
+        // container.appendChild(bar);
+        let canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(10,10,10,100);
+        container.appendChild(canvas);
+//         <canvas id="myCanvas" width="10" height="100" style="border:1px solid #000000;">
+// Your browser does not support the HTML canvas tag.
+// </canvas>
+
+
     }
 }
-showbars();
-function play(){
+showbars(); 
+function play(){ //this function colorises the bars according to time lapsed
     
     // console.log('play function');
     let bars = document.querySelectorAll('#container div');
@@ -35,12 +45,12 @@ function play(){
         }
     }
 }
-var interval;
+var intervalID;
 function start_pause(){
     // console.log(interval,'intervalID');
-    if(interval==undefined){
-        start_pause_button.innerHTML = `<i class="fa fa-pause" style="font-size:36px;"></i>`
-        interval = setInterval(()=>{
+    if(intervalID==undefined){ //if intervalID is undefined, we shall set a new interval
+        start_pause_button.innerHTML = `<i class="fa fa-pause" style="font-size:36px;"></i>` //changing start/pause icon
+        intervalID = setInterval(()=>{
             time++;
             // console.log(time);
             if(time>90){
@@ -51,13 +61,13 @@ function start_pause(){
             }
         },1000)    
     }
-    else{
-        clearInterval(interval)       
-        interval = undefined;
-        start_pause_button.innerHTML = `<i  class="fa fa-play" style="font-size:36px;"></i>`
+    else{ //if the interval is already set, then we clear it and set IntervalID as undefined
+        clearInterval(intervalID)       
+        intervalID = undefined;
+        start_pause_button.innerHTML = `<i  class="fa fa-play" style="font-size:36px;"></i>` //changing start pause icon
     }
     
 }
 function shift(e){
-    time = e.target.id-1;
+    time = e.target.id-1; //on click over the bars, the value of time lapsed is changed
 }
